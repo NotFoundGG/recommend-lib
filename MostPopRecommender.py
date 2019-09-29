caa19a7e08a88c845cdb4f2f51387d8229523112
yudi <yudi@shanshu.ai>
2019-09-29 16:04:57 +0800

    fixed bug

--- MostPopRecommender.py
+++ MostPopRecommender.py
@@ -2,10 +2,10 @@
 @Author: Yu Di
 @Date: 2019-09-29 10:54:40
 @LastEditors: Yudi
-@LastEditTime: 2019-09-29 14:56:55
+@LastEditTime: 2019-09-29 16:04:06
 @Company: Cardinal Operation
 @Email: yudi@shanshu.ai
-@Description: 
+@Description: Popularity-based recommender
 '''
 import numpy as np
 import pandas as pd
@@ -22,24 +22,35 @@ class MostPopRecommender(object):
     def fit(self, df):
         '''most popular item'''
         res = df['item'].value_counts()
-        self.top_n = res[:self.N].index.tolist()
+        # self.top_n = res[:self.N].index.tolist()
+        self.rank_list = res.index.tolist()
 
     def predict(self, df):
+        item_list = df['item'].unique().tolist()
+        exists_item = [i for i in self.rank_list if i in item_list]
+        non_exists = list(set(item_list) - set(exists_item))
+        np.random.shuffle(non_exists)
+
+        if len(exists_item) == 0:
+            exists_item = []
+        if len(non_exists) == 0:
+            non_exists = []
+        rank_list = exists_item + non_exists
+        top_n = rank_list[:self.N]
         res = defaultdict(list)
         for u in df.user.unique():
-            res[u] = self.top_n
+            res[u] = top_n
         return res
 
 if __name__ == '__main__':
     #TODO top-N setting
-    k = 5
+    k = 10
 
     df = load_rate('ml100k')
     train_set, test_set = train_test_split(df, test_size=0.2, random_state=2019)
 
     reco = MostPopRecommender(k)
     reco.fit(train_set)
-
     # get top-N list for test users
     preds = reco.predict(test_set)
     # get actual interaction info. of test users
